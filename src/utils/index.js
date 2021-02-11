0c3bf3e3f51aba64dc27e5ea87ffe42efd35c80e
stevenjchang <stevenjchang@gmail.com>
2021-02-11 01:01:31 -0800

    add cmd for node --experimental-modules

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+// input: 1 character string that is a valid hexadecimal character
+// output: a number between 0 and 15
+export function toDecimal() {}
+
+//input 2 decimals
+//output: sum of the 2 decimals
+export function toHex() {}
